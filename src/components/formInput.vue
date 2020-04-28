cdd6c550fd206c544c5759b64fe52f9055e1295e
atsushi570 <55192393+atsushi570@users.noreply.github.com>
2020-04-28 23:45:38 +0900

    UIの一部をコンポーネント化

---
+++ src/components/formInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="form-wrapper">
+    <div class="columns is-desktop">
+      <div class="field column is-6 is-offset-3">
+        <label class="label">{{ data.label }}</label>
+        <p class="control">
+          <input
+            class="input"
+            :type="data.type"
+            :placeholder="data.placeHolder"
+          />
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      required: true,
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped>
+.field {
+  padding-right: 4vw;
+  padding-left: 4vw;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,51 +3,47 @@
     <div class="container my-m">
       <h2 class="header-text">ログイン画面</h2>
 
-      <div class="form-wrapper">
-        <div class="columns is-desktop">
-          <div class="field column is-6 is-offset-3">
-            <label class="label">ユーザ名</label>
-            <p class="control">
-              <input class="input" type="text" placeholder="Your Name" />
-            </p>
-          </div>
-        </div>
+      <formInput :data="formUserName"></formInput>
+      <formInput :data="formEmail"></formInput>
+      <formInput :data="formPassword"></formInput>
 
-        <div class="columns is-desktop">
-          <div class="field column is-6 is-offset-3">
-            <label class="label">メールアドレス</label>
-            <p class="control">
-              <input
-                class="input"
-                type="email"
-                placeholder="your.email.address@example.com"
-              />
-            </p>
-          </div>
-        </div>
+      <div class="has-text-centered mt-s">
+        <button class="button is-info is-outlined">ログイン</button>
+      </div>
 
-        <div class="columns is-desktop">
-          <div class="field column is-6 is-offset-3">
-            <label class="label">パスワード</label>
-            <p class="control">
-              <input class="input" type="password" placeholder="Password" />
-            </p>
-          </div>
-        </div>
-
-        <div class="has-text-centered">
-          <button class="button is-info is-outlined">ログイン</button>
-        </div>
-
-        <div class="has-text-centered">
-          <a class="has-text-info register-link" href="">新規登録はこちら</a>
-        </div>
+      <div class="has-text-centered">
+        <a class="has-text-info register-link" href="">新規登録はこちら</a>
       </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import formInput from '~/components/formInput.vue'
+
+export default {
+  components: { formInput },
+  data() {
+    return {
+      formUserName: {
+        label: 'ユーザ名',
+        type: 'text',
+        placeHolder: 'Your Name'
+      },
+      formEmail: {
+        label: 'メールアドレス',
+        type: 'Email',
+        placeHolder: 'your.email@example.com'
+      },
+      formPassword: {
+        label: 'パスワード',
+        type: 'password',
+        placeHolder: 'Passwrod'
+      }
+    }
+  }
+}
+</script>
 
 <style scoped>
 .register-link {
