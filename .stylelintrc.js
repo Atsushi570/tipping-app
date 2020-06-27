dd83bde7db3a98bbc182d21473a8e7efe1d3a5db
atsushi570 <55192393+atsushi570@users.noreply.github.com>
2020-06-27 14:27:32 +0900

    stylelintにscssの設定を追加

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
   extends: [
+    'stylelint-config-recommended-scss',
     'stylelint-config-standard',
     'stylelint-config-prettier',
     'stylelint-config-recess-order'
-  ]
+  ],
+  rules: {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true
+  }
 }
