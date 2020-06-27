60b9bf0ebca470ed307d43e75c7d780ac9d8f935
atsushi570 <55192393+atsushi570@users.noreply.github.com>
2020-06-27 21:06:47 +0900

    atomsの追加

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button
+      :class="['button', colors ? colors : 'is-primary']"
+      @click="clickHandler"
+    >
+      <slot>
+        <span class="test">test</span>
+        {{ btnText }}
+      </slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    btnText: String,
+    colors: String
+  },
+  methods: {
+    clickHandler(event) {
+      this.$emit('click', event)
+    }
+  }
+}
+</script>
---
+++ src/components/atoms/Text.vue
@@ -0,0 +1,58 @@
+<template>
+  <component
+    :is="level || 'p'"
+    :preWrap="preWrap"
+    :noSpace="noSpace"
+    :class="[
+      'text',
+      'basics-font-size-space-' +
+        (level === 'p' || 'label' ? fontSize || 'middle' : level),
+      'basics-font-color-' + (fontColor || 'yellow'),
+      'basics-align-' + (align || 'center')
+    ]"
+  >
+    <span>
+      <slot>
+        <span>{{ text }}</span>
+      </slot>
+    </span>
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    level: String,
+    preWrap: Boolean,
+    noSpace: Boolean,
+    fontSize: String,
+    fontColor: String,
+    align: String,
+    text: String
+  },
+  computed: {
+    leftSpace() {
+      return (
+        'padding-left: ' + (this.fontSize === 'small' ? '8px' : '16px') + ';'
+      )
+    },
+    rightSpace() {
+      return (
+        'padding-right: ' + (this.fontSize === 'small' ? '8px' : '16px') + ';'
+      )
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.text {
+  &[preWrap] {
+    white-space: pre-wrap;
+  }
+
+  &[noSpace] {
+    padding-bottom: 0;
+  }
+}
+</style>
