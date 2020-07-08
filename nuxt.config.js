25c184c46a198841cc4d154c6c668b10d3d19650
atsushi570 <55192393+atsushi570@users.noreply.github.com>
2020-07-08 22:36:03 +0900

    Storybookを追加したときにcore-jsがアップデートされてNuxtに影響が出た対応のため修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,6 +97,20 @@ export default {
       config.node = {
         fs: 'empty'
       }
+    },
+    babel: {
+      presets({ isServer }) {
+        return [
+          [
+            require.resolve('@nuxt/babel-preset-app'),
+            // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
+            {
+              buildTarget: isServer ? 'server' : 'client',
+              corejs: { version: 3 }
+            }
+          ]
+        ]
+      }
     }
   },
   env: {
