92d68b245ef77315c488c1128266540dbabec765
atsushi570 <55192393+atsushi570@users.noreply.github.com>
2020-07-08 22:37:12 +0900

    storyshotsを追加

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,15 @@
 module.exports = {
+  verbose: true,
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest'
   },
+  transformIgnorePatterns: ['/node_modules/(?!(@storybook/.*\\.vue$))'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^~/(.*)$': '<rootDir>/src/$1'
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js'
   },
-  moduleFileExtensions: ['js', 'json', 'vue']
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  setupFiles: ['<rootDir>.jest/setup.js']
 }
