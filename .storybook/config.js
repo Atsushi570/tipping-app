bc6d0a498cd1dc0864ec7f6d077fb333cfb0ce28
atsushi570 <55192393+atsushi570@users.noreply.github.com>
2020-07-08 21:40:37 +0900

    storyshots導入のためstorybooのbulma読み込み方法を変更

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,17 +1,5 @@
-import { configure, addDecorator } from '@storybook/vue'
-import CommonCss from './Common.Css'
-
-// bulmaなど共通で読み込みたいcssを読み込んでいるコンポーネントを全てのstoriesに注入する
-addDecorator(() => ({
-  components: { CommonCss },
-  render() {
-    return (
-      <common-css>
-        <story slot="story"></story>
-      </common-css>
-    )
-  }
-}))
+import { configure } from '@storybook/vue'
+import 'bulma/css/bulma.css'
 
 // storiesディレクトリにある*.stories.jsファイルを読み込む
 configure(require.context('../src/stories', true, /\.stories\.js$/), module)
